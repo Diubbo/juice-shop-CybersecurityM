5702899a467480ff8a4f235d8c1155f8d529edd8
Mattia Pasquali <74003821+009mattia@users.noreply.github.com>
2025-03-08 14:43:07 +0100

    Update cypress.config.ts

--- cypress.config.ts
+++ cypress.config.ts
@@ -6,7 +6,7 @@ import * as utils from './lib/utils'
 import * as otplib from 'otplib'
 
 export default defineConfig({
-  projectId: '3hrkhu',
+  projectId: 'rpze27',
   defaultCommandTimeout: 10000,
   e2e: {
     baseUrl: 'http://localhost:3000',
